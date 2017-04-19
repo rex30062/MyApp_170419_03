82fa564cd205f454edebdb99572107cb03a07fc3
Cclz <rex620620>
2017-04-19 13:56:57 +0800

    PreferenceActivity

--- app/src/main/java/com/cclz/myapp_170419_03/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170419_03/MainActivity.java
@@ -1,9 +1,13 @@
 package com.cclz.myapp_170419_03;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.audiofx.BassBoost;
 import android.opengl.ETC1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -35,5 +39,20 @@ public class MainActivity extends AppCompatActivity {
         EditText ed=(EditText)findViewById(R.id.editText);
         editor.putString("username", ed.getText().toString());
         editor.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Settings");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getTitle().equals("Settings")){
+            Intent it=new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(it);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
