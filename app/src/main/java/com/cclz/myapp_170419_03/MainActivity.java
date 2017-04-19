553ae6336fe08ceb968311712105ea7f3777c11f
Cclz <rex620620>
2017-04-19 13:26:40 +0800

    more practical

--- app/src/main/java/com/cclz/myapp_170419_03/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170419_03/MainActivity.java
@@ -1,9 +1,12 @@
 package com.cclz.myapp_170419_03;
 
 import android.content.SharedPreferences;
+import android.opengl.ETC1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +17,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences sp=getSharedPreferences("mydata", MODE_PRIVATE);
-        SharedPreferences.Editor editor=sp.edit();
-        editor.putString("username", "Peter");
-        editor.commit();
+        String str=sp.getString("username", "");
+        EditText ed=(EditText)findViewById(R.id.editText);
+        ed.setText(str);
 
     }
     public void clickRead(View v){
         SharedPreferences sp=getSharedPreferences("mydata", MODE_PRIVATE);
         String str=sp.getString("username", "");
-        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+        TextView tv=(TextView)findViewById(R.id.textView);
+        tv.setText(str);
+    }
+
+    public void clickWrite(View v){
+        SharedPreferences sp=getSharedPreferences("mydata",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sp.edit();
+        EditText ed=(EditText)findViewById(R.id.editText);
+        editor.putString("username", ed.getText().toString());
+        editor.commit();
     }
 }
